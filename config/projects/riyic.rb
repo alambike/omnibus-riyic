e8b26ee51a7f7be9067f30ca4b64de0dc34a22c0
root <root@omnibus.(none)>
2013-12-13 00:43:34 +0000

    Initial commit

---
+++ config/projects/riyic.rb
@@ -0,0 +1,26 @@
+name "riyic"
+maintainer "Riyic"
+homepage "riyic.com"
+
+replaces        "riyic"
+install_path    "/opt/riyic"
+build_version   Omnibus::BuildVersion.new.semver
+build_iteration 1
+
+# creates required build directories
+dependency "preparation"
+
+# riyic dependencies/components
+# dependency "somedep"
+
+# version manifest file
+dependency "version-manifest"
+
+dependency "chef"
+dependency "ohai" if ENV["OHAI_GIT_REV"]
+dependency "berkshelf"
+dependency "test-kitchen"
+dependency "foodcritic"
+
+exclude "\.git*"
+exclude "bundler\/git"
