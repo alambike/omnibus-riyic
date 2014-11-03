9b0dd18004aa4426ba6da60c1524fabe58c78b3b
J. Gomez <alambike@gmail.com>
2014-11-03 03:10:16 +0100

    Creamos o noso paquete de instalacion de chef con varias modificacions: - por un lado deixamolo na ruta standard /opt/chef - agregamos as gemas e as librerias de cliente de mysql e postgresql - correximos o exclude para que non garde os archivos de cache de bundle

--- config/projects/riyic.rb
+++ config/projects/riyic.rb
@@ -4,25 +4,49 @@ friendly_name "Riyic Client"
 maintainer 'Javier Gomez <alambike@gmail.com>'
 homepage 'https://riyic.com'
 
-install_dir     "/opt/riyic"
-build_version   Omnibus::BuildVersion.semver
-build_iteration 1
+install_dir     "/opt/chef"
 
-override :ruby, version: "2.1.3"
-override :'chef-gem', version: "11.16.4"
+build_iteration 1
+build_version do
+  # Use chef to determine the build version
+  source :git, from_dependency: 'chef'
+
+  # Output a SemVer compliant version string
+  output_format :semver
+end
+
+#override :cacerts, version: '2014.08.20'
+override :bundler,        version: "1.7.2"
+override :ruby,           version: "2.1.4"
+override :rubygems,       version: "2.4.1"
+
+override :chef, version: "11.16.4"
 
 # creates required build directories
 dependency 'preparation'
 
 # riyic dependencies/components
-dependency "chef-gem"
-dependency "ohai" if ENV["OHAI_GIT_REV"]
+dependency "chef"
 
-#dependency "berkshelf"
+# instalamos a nosa gema
 dependency "lambom"
+
+# instalamos a gema de mysql
+dependency "mysql-client"
+dependency "mysql-gem"
+
+# instalamos a gema de postgresql
+override :postgresql, 	version: "9.3.4"
+dependency "postgresql"
+dependency "pg-gem"
+
 
 # version manifest file
 dependency 'version-manifest'
 
-exclude '\.git*'
-exclude 'bundler\/git'
+exclude '**/.git'
+exclude '**/bundler/git'
+exclude '**/bundler/gems'
+
+#exclude '**/share'
+
