b21d4ef9d28886881ffc0d1f992ab381e768ae16
J. Gomez <alambike@gmail.com>
2014-11-03 03:07:07 +0100

    Excluimos directorios de cache

--- config/projects/riyic.rb
+++ config/projects/riyic.rb
@@ -1,18 +1,20 @@
 
 name 'riyic'
+friendly_name "Riyic Client"
 maintainer 'Javier Gomez <alambike@gmail.com>'
 homepage 'https://riyic.com'
 
-install_dir     '/opt/riyic'
+install_dir     "/opt/riyic"
 build_version   Omnibus::BuildVersion.semver
 build_iteration 1
+
+override :ruby, version: "2.1.3"
+override :'chef-gem', version: "11.16.4"
 
 # creates required build directories
 dependency 'preparation'
 
 # riyic dependencies/components
-# seteamos versiond e chef
-ENV["CHEF_GIT_REV"] = "11-stable"
 dependency "chef-gem"
 dependency "ohai" if ENV["OHAI_GIT_REV"]
 
--- config/software/lambom.rb
+++ config/software/lambom.rb
@@ -1,5 +1,10 @@
 name "lambom"
-dependencies ["ruby", "rubygems","curl"]
+default_version "get_rid_of_berkshelf"
+relative_path "lambom"
+
+dependency "ruby"
+dependency "rubygems"
+dependency "curl"
 
 always_build true
 
@@ -11,6 +16,8 @@ end
 build do
   #gem "install #{name} -n #{install_dir}/bin --no-rdoc --no-ri -v #{version}"
   #gem "install #{name} -n #{install_dir}/bin --no-rdoc --no-ri"
+  gem "install ruby-shadow -n #{install_dir}/bin --no-ri --no-rdoc", 
+	:env => {"PATH" => "#{install_dir}/embedded/bin:#{ENV['PATH']}"}
 
   gem "build lambom.gemspec"
   gem ["install lambom*.gem",
